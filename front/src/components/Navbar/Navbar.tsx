6f07669bed309f064c94679ac9fa40bc161329e5
PortoPedro <pedro20049955080800@gmail.com>
2024-11-28 19:43:10 -0300

    voltar codigo original

---
+++ front/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import { IoGitMergeOutline } from "react-icons/io5";
+import { RiTeamFill } from "react-icons/ri";
+import { FaTruckMoving } from "react-icons/fa";
+import { FaPeopleCarry } from "react-icons/fa";
+import { FaMap } from "react-icons/fa6";
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+import * as styled from './Navbar.styles';
+import useNavbar from './Navbar.hook';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    label: 'Fluxo',
+    key: 'route',
+    icon: <IoGitMergeOutline />,
+  },
+  {
+    label: 'Funcionarios',
+    key: 'funcionarios',
+    icon: <RiTeamFill />,
+  },
+  {
+    label: 'Frota',
+    key: 'veiculos',
+    icon: <FaTruckMoving />,
+  },
+  {
+    label: 'Serviços',
+    key: 'servicos',
+    icon: <FaPeopleCarry />,
+  },
+  {
+    label: 'Mapa de Acompanhamento',
+    key: 'mapa',
+    icon: <FaMap />,
+  },
+];
+
+function Navbar() {
+    const {current, onClick} = useNavbar();
+  
+    return (
+        <styled.NavbarContainer>
+            <styled.Logo src="logo_amitran.png" alt="logo amitran" />
+            <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} style={{justifyContent: 'center'}}/>
+        </styled.NavbarContainer>
+    )
+}
+
+export default Navbar;
